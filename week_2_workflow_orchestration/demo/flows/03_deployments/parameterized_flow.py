f4ddf1d3e2c67531fe490430d8ed0472fb22a0ce
Martin Uribe <clamytoe@gmail.com>
2023-02-03 19:24:30 -0600

    Addd prefect ignore file and parameterized flow script

--- week_2_workflow_orchestration/demo/flows/03_deployments/parameterized_flow.py
+++ week_2_workflow_orchestration/demo/flows/03_deployments/parameterized_flow.py
@@ -1,18 +1,15 @@
+from datetime import timedelta
 from pathlib import Path
+
 import pandas as pd
 from prefect import flow, task
-from prefect_gcp.cloud_storage import GcsBucket
-from random import randint
 from prefect.tasks import task_input_hash
-from datetime import timedelta
+from prefect_gcp.cloud_storage import GcsBucket
 
 
 @task(retries=3, cache_key_fn=task_input_hash, cache_expiration=timedelta(days=1))
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Read taxi data from web into pandas DataFrame"""
-    # if randint(0, 1) > 0:
-    #     raise Exception
-
     df = pd.read_csv(dataset_url)
     return df
 
@@ -31,7 +28,9 @@ def clean(df: pd.DataFrame) -> pd.DataFrame:
 @task()
 def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     """Write DataFrame out locally as parquet file"""
-    path = Path(f"data/{color}/{dataset_file}.parquet")
+    local_dir = Path("data", color)
+    local_dir.mkdir(parents=True, exist_ok=True)
+    path = local_dir / f"{dataset_file}.parquet"
     df.to_parquet(path, compression="gzip")
     return path
 
@@ -39,8 +38,8 @@ def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
 @task()
 def write_gcs(path: Path) -> None:
     """Upload local parquet file to GCS"""
-    gcs_block = GcsBucket.load("zoom-gcs")
-    gcs_block.upload_from_path(from_path=path, to_path=path)
+    gcs_block = GcsBucket.load("dtc-de-gcs")
+    gcs_block.upload_from_path(from_path=path, to_path=path)  # type: ignore
     return
 
 
@@ -58,7 +57,9 @@ def etl_web_to_gcs(year: int, month: int, color: str) -> None:
 
 @flow()
 def etl_parent_flow(
-    months: list[int] = [1, 2], year: int = 2021, color: str = "yellow"
+    months: list[int] = [1, 2],
+    year: int = 2021,
+    color: str = "yellow",
 ):
     for month in months:
         etl_web_to_gcs(year, month, color)
@@ -66,6 +67,7 @@ def etl_parent_flow(
 
 if __name__ == "__main__":
     color = "yellow"
+    year = 2021
     months = [1, 2, 3]
     year = 2021
     etl_parent_flow(months, year, color)
